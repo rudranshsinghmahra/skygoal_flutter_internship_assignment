6dcb20465762d49492408f607d972df6bf4c8bd2
Rudransh Singh Mahra <rs.mahra4546@gmail.com>
2022-12-12 19:55:13 +0530

    Second Home Page Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skygoal_internship_assignment_rudransh_singh/screens/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:skygoal_internship_assignment_rudransh_singh/widgets/custom_appbar.dart';
 import 'package:skygoal_internship_assignment_rudransh_singh/widgets/custom_banners.dart';
@@ -21,6 +22,8 @@ class _HomePageState extends State<HomePage>
     super.initState();
   }
 
+  String? groupValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,19 +41,83 @@ class _HomePageState extends State<HomePage>
           child: SingleChildScrollView(
             child: Column(
               children: [
-                customBanner(
-                  "Top Colleges",
-                  "Search through thousands of accredited colleges and universities online to find the right one for you.  Apply in 3 min, and connect with your future.",
-                  "assets/banner_1.png",
-                  134.0,
-                  "+126",
-                  "College",
-                  0.0,
+                GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30),
+                          topLeft: Radius.circular(30),
+                        ),
+                      ),
+                      context: context,
+                      builder: (context) {
+                        return StatefulBuilder(
+                          builder: (context, setTheState) {
+                            return SizedBox(
+                              height: 350,
+                              width: MediaQuery.of(context).size.width,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 28.0, left: 38, right: 38),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "Sort by",
+                                          style: GoogleFonts.lato(
+                                              fontWeight: FontWeight.w800,
+                                              fontSize: 18),
+                                        ),
+                                        GestureDetector(
+                                            onTap: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Icon(Icons.cancel))
+                                      ],
+                                    ),
+                                    const Divider(
+                                      color: Colors.grey,
+                                    ),
+                                    const SizedBox(
+                                      height: 30,
+                                    ),
+                                    reusableFilter("Bachelor of Technology",
+                                        "assets/education.png", setTheState),
+                                    reusableFilter("Bachelor of Architecture",
+                                        "assets/sketch.png", setTheState),
+                                    reusableFilter("Pharmacy",
+                                        "assets/pharmacy.png", setTheState),
+                                    reusableFilter("Law", "assets/balance.png",
+                                        setTheState),
+                                    reusableFilter("Management",
+                                        "assets/management.png", setTheState),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                  child: customBanner(
+                    "Top Colleges",
+                    "Search through thousands of accredited colleges and universities online to find the right one for you.  Apply in 3 min, and connect with your future.",
+                    "assets/banner_1.png",
+                    134.0,
+                    "+126",
+                    "College",
+                    0.0,
+                  ),
                 ),
                 customBanner(
                     "Top Schools",
                     "Searching for the best school for you just got easier! With our Advanced School Search, you can easily filter out the schools that are fit for you.",
-                    "assets/banner_1.png",
+                    "assets/banner_2.png",
                     134.0,
                     "+106",
                     "Schools",
@@ -58,7 +125,7 @@ class _HomePageState extends State<HomePage>
                 customBanner(
                     "Exams",
                     "Find an end to end information about the exams that are happening in India",
-                    "assets/banner_1.png",
+                    "assets/banner_3.png",
                     134.0,
                     "+16",
                     "Exams   ",
@@ -151,6 +218,52 @@ class _HomePageState extends State<HomePage>
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget reusableFilter(String filterName, leadingImage, setTheState) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 25,
+                  width: 25,
+                  child: Image.asset(
+                    leadingImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  filterName,
+                  style: GoogleFonts.lato(
+                      fontWeight: FontWeight.w600, fontSize: 16),
+                )
+              ],
+            ),
+          ),
+          Radio(
+            activeColor: const Color(0xff0E3C6E),
+            visualDensity: const VisualDensity(
+                horizontal: VisualDensity.minimumDensity,
+                vertical: VisualDensity.minimumDensity),
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            value: filterName,
+            groupValue: groupValue,
+            onChanged: (value) {
+              setTheState(() {
+                groupValue = value.toString();
+              });
+            },
+          ),
+        ],
       ),
     );
   }
